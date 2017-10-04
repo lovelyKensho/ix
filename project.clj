93a3e57cddfa3d40e439e20832cff823cd1b6055
Alexander Guerra <alex@lovelykensho.com>
2017-10-03 23:52:27 -0500

    Add website to project file

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject ix "0.0.0-SNAPSHOT"
-  :description "A humble experiment..."
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot ix.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+            :description "A humble experiment..."
+            :url "https://github.com/lovelyKensho/ix"
+            :license {:name "Eclipse Public License"
+                      :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :dependencies [[org.clojure/clojure "1.8.0"]]
+            :main ^:skip-aot ix.core
+            :target-path "target/%s"
+            :profiles {:uberjar {:aot :all}})
