28bc3af50ea034b2419bb535076cbbf76b93b0a4
Alexander Guerra <alex@lovelykensho.com>
2017-10-03 22:20:00 -0500

    Change the version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ix "0.1.0-SNAPSHOT"
+(defproject ix "0.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
