a40c091028d77b2b9d2016acc4275040c4ea1b68
Alexander Guerra <alex@lovelykensho.com>
2017-10-06 19:45:11 -0500

    Add content to core file

--- src/ix/core.clj
+++ src/ix/core.clj
@@ -1,12 +1,36 @@
 (ns ix.core
   (:gen-class))
+
 ;; --- The start ---
 
 (def first-name "Alexander")
-;; --- The end ---
+(def last-name "Guerra")
 
-;; --- Notes ---
+;; Things I want to do before I die...
+(def bucket nul)
 
+(def musical-groups ["The Beatles"
+                     "The Clash"
+                     "Buzzcocks"])
+
+(def foods ["chicken noodle soup"])
+
+(def pearls-of-wisdom ["Do not mutate the state"
+                       "Wallet keys phone"
+                       "Mad dogs and Englishmen..."
+                       "Always have a scratch monkey"])
+
+(def drinks ["beer"
+             "wine"
+             "Dr. Pepper"])
+
+(def actors nil)
+
+(def boy-names ["Alexander"
+                "John"
+                "Adam"])
+
+(def stop-words nil)
 
 (defn -main
   "I don't do a whole lot ... yet."
