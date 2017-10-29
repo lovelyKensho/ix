8a177c3accdef5893e969b531571f7d0a7c22066
Alexander Guerra <alex@lovelykensho.com>
2017-10-29 17:44:10 -0500

    Add overtone as a dependency

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.8.0"]
                            [proto-repl "0.3.1"]
-                           [proto-repl-charts "0.3.2"]]
+                           [proto-repl-charts "0.3.2"]
+                           [overtone "0.10.3"]]
             :main ^:skip-aot ix.core
             :target-path "target/%s"
             :profiles {:uberjar {:aot :all}})
