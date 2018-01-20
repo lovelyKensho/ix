30c38ec758b47c5ea35e566ea52ee7b225c5e3f2
Alexander Guerra <alex@lovelykensho.com>
2018-01-19 22:38:47 -0600

    Update Clojure and add proto-repl

--- project.clj
+++ project.clj
@@ -3,8 +3,9 @@
             :url "https://github.com/lovelyKensho/ix"
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.8.0"]
-                           [overtone "0.10.3"]]
+            :dependencies [[org.clojure/clojure "1.9.0"]
+                           [overtone "0.10.3"]
+                           [proto-repl "0.3.1"]]
             :main ^:skip-aot ix.core
             :target-path "target/%s"
             :profiles {:uberjar {:aot :all}})
