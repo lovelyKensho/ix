53f7526a53690eedae7798fe8620df89868fd8b5
Alexander A. Guerra <alex@lovelyKensho.com>
2018-01-22 19:28:44 -0600

    Start adding content

--- src/ix/data.clj
+++ src/ix/data.clj
@@ -1,1 +1,10 @@
 (ns ix.data)
+
+(def first-name "Alexander")
+(def last-name "Guerra")
+
+;; Just have fun with it...
+(def super-hero-names ["Super Ada"])
+
+(def foods ["Soup"
+            "Sushi"])
---
+++ src/ix/todo.clj
@@ -0,0 +1,15 @@
+(ix.todo)
+
+;; Just to keep everything organized
+
+(def todos ["Make lists"
+            "Get auto-indenting to work"
+            "Think: How should a todo list work"
+            "Make idea system"
+            "Comments should not be italic"])
+
+(def clean-house-todo {:name "Clean house"
+                       :date-due 123
+                       :completed? false})
+
+;; Everything is data
