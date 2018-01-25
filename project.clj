7bc338a9ed99d58b7c8f7606d432f83a6dcf7537
Alexander A. Guerra <alex@lovelyKensho.com>
2018-01-24 20:32:02 -0600

    Add clj-http and cheshire to project.clj

--- project.clj
+++ project.clj
@@ -5,8 +5,9 @@
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.9.0"]
                            [overtone "0.10.3"]
-                           [proto-repl "0.3.1"]]
+                           [proto-repl "0.3.1"]
+                           [clj-http "3.7.0"]
+                           [cheshire "5.8.0"]]
             :main ^:skip-aot ix.core
             :target-path "target/%s"
             :profiles {:uberjar {:aot :all}})
-
