d08e68e3e043c61a1073e116c3a373e7c8417b62
Alexander A. Guerra <alex@lovelyKensho.com>
2018-01-26 09:06:37 -0600

    Add lein-gorilla to the project.clj file

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
                            [overtone "0.10.3"]
                            [proto-repl "0.3.1"]
                            [clj-http "3.7.0"]
-                           [cheshire "5.8.0"]]
+                           [cheshire "5.8.0"]
+                           [lein-gorilla "0.4.0"]]
             :main ^:skip-aot ix.core
             :target-path "target/%s"
             :profiles {:uberjar {:aot :all}})
